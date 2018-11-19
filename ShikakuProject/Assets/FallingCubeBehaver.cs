20da56538a0e155630dda7024d0e6798665b8ec5
kumesan <al17042@shibaura-it.ac.jp>
2018-11-19 20:27:33 +0900

    before_add_FallingCube

---
+++ ShikakuProject/Assets/FallingCubeBehaver.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingCubeBehaver : MonoBehaviour {
+
+    private CharacterController controller;
+
+	// Use this for initialization
+	void Start () {
+        controller = GetComponent<CharacterController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
