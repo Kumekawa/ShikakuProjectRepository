9fff9584e981a43e3ef712fffef48c193631a814
kumesan <al17042@shibaura-it.ac.jp>
2018-11-20 09:54:47 +0900

    Add_FallingCube
    落下する立方体を追加。
    次にランダムに色を付ける作業を行う

--- ShikakuProject/Assets/FallingCubeBehaver.cs
+++ ShikakuProject/Assets/FallingCubeBehaver.cs
@@ -2,17 +2,55 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FallingCubeBehaver : MonoBehaviour {
+public class FallingCubeBehaver : MonoBehaviour
+{
 
-    private CharacterController controller;
+	public float MoveSpeed;
+
+	private CharacterController controller;
+	private Vector3 moveDirection;
+	private GameObject MainCube;
+	private Vector3 MainCubePosition;
+
+	private float zPosition;
 
 	// Use this for initialization
-	void Start () {
-        controller = GetComponent<CharacterController>();
+	void Start()
+	{
+		controller = GetComponent<CharacterController>();
+		MainCube = GameObject.Find("MainCube");
+		MainCubePosition = GameObject.Find("MainCube").transform.position;
 	}
-	
+
 	// Update is called once per frame
-	void Update () {
-		
+	void Update()
+	{
+		Move();
+		FixZPosition();
+
+	}
+
+	Vector3 MoveDirection()
+	{
+		return new Vector3(0f, -MoveSpeed, 0f);
+	}
+
+	void Move()
+	{
+		controller.Move(MoveDirection() * Time.deltaTime);
+	}
+
+	void FixZPosition()
+	{
+		Vector3 temp = transform.position;
+		if (MainCube.transform.position.y - MainCube.transform.localScale.y / 2 < transform.position.y +transform.localScale.y / 2)
+		{
+			temp.z = 10f;
+		}
+		else
+		{
+			temp.z = 0f;
+		}
+		transform.position = temp;
 	}
 }
